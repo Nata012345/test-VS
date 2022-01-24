57bf3de562febfbcee2c9b51ba9041169a130068
Nata012345 <seredanatasha@gmail.com>
2022-01-25 00:58:02 +0300

    feat: burger menu

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// function hamburgerMenu(selector) {
-//   let menu = $(selector);
-//   let button = menu.find('.hamburger-menu__button');
-//   let links = menu.find('.hamburger-navigation__link');
-//   let overlay = menu.find('.hamburger-menu__overlay');
+function hamburgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.hamburger-menu__button');
+  let links = menu.find('.hamburger-navigation__link');
+  let overlay = menu.find('.hamburger-menu__overlay');
 
-//   button.on('click', (e) => {
-//     e.preventDefault();
-//     toggleMenu();
-//   });
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
 
-//   links.on('click', () => toggleMenu());
-//   overlay.on('click', () => toggleMenu());
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
 
-//   function toggleMenu() {
-//     menu.toggleClass('hamburger-menu_active');
+  function toggleMenu() {
+    menu.toggleClass('hamburger-menu_active');
 
-//     if (menu.hasClass('hamburger-menu_active')) {
-//       $('body').css('overflow', 'hidden');
+    if (menu.hasClass('hamburger-menu_active')) {
+      $('body').css('overflow', 'hidden');
 
-//     } else {
-//       $('body').css('overflow', 'visible');
-//     }
-//   }
-// }
+    } else {
+      $('body').css('overflow', 'visible');
+    }
+  }
+}
 
-// hamburgerMenu('.hamburger-menu');
+hamburgerMenu('.hamburger-menu');
