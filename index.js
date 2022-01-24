7485ec28ca002094b0f89ecfba9ad5fb61960d2b
Nata012345 <seredanatasha@gmail.com>
2022-01-24 22:39:27 +0300

    feat: add burrger-menu

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
-  $('.hamburger').click(function(event) {
-    $('.hamburger', '.header-right-container').toggleClass('active');
-  })
-})
+// function hamburgerMenu(selector) {
+//   let menu = $(selector);
+//   let button = menu.find('.hamburger-menu__button');
+//   let links = menu.find('.hamburger-navigation__link');
+//   let overlay = menu.find('.hamburger-menu__overlay');
+
+//   button.on('click', (e) => {
+//     e.preventDefault();
+//     toggleMenu();
+//   });
+
+//   links.on('click', () => toggleMenu());
+//   overlay.on('click', () => toggleMenu());
+
+//   function toggleMenu() {
+//     menu.toggleClass('hamburger-menu_active');
+
+//     if (menu.hasClass('hamburger-menu_active')) {
+//       $('body').css('overflow', 'hidden');
+
+//     } else {
+//       $('body').css('overflow', 'visible');
+//     }
+//   }
+// }
+
+// hamburgerMenu('.hamburger-menu');
